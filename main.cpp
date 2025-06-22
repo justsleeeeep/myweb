3c69e91b0e13fd3065e873276f1d2a8853955983
justsleeeeep <qc87672090@qq.com>
2025-06-22 22:09:38 +0800

    First commit: My Simple C++ App

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream> // 引入输入输出流库
+#include <string>   // 引入字符串库
+
+int main()
+{
+    std::cout << "Hello from My Simple C++ App!" << std::endl;
+    std::cout << "This app is running on the server!" << std::endl;
+
+    // 可以在这里添加一些简单的用户交互，让程序不会立即退出
+    // std::string input;
+    // std::cout << "Press Enter to exit..." << std::endl;
+    // std::getline(std::cin, input);
+
+    return 0; // 程序正常退出
+}
