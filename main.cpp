87aa8dc20fdffb08abdaa85409da10b06b3ac52b
justsleeeeep <qc87672090@qq.com>
2025-06-29 13:49:11 +0800

    1

--- main.cpp
+++ main.cpp
@@ -6,11 +6,17 @@ int main() {
 
     // 定义一个路由：当浏览器访问根路径 "/" 时，执行以下操作
     svr.Get("/", [](const httplib::Request& req, httplib::Response& res) {
-        res.set_content("<h1>Hello from C++ Web Server!</h1><p>This is QC first website.</p>", "text/html");
+        // 设置响应内容类型为 JSON
+        res.set_header("Content-Type", "application/json");
+
+        // 构造一个 JSON 字符串。
+        std::string json_response_body = R"({"message": "Hello from C++ Web Server!", "data": {"name": "YourName", "age": 25}})";
+
+        // 发送 JSON 响应
+        res.set_content(json_response_body, "application/json");
     });
 
     // 启动服务器，监听所有可用 IP 地址的 8080 端口
-    // 我们选择 8080 端口，因为 80 端口通常需要 root 权限，并且 Nginx 会监听 80
     std::cout << "C++ Web Server starting on port 8080..." << std::endl;
     svr.listen("0.0.0.0", 8080);
 
