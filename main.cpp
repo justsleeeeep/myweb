945adb465fac1e0c7cf420bbbabf4781f3e312b3
justsleeeeep <qc87672090@qq.com>
2025-06-29 09:51:54 +0800

    change name

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
 
     // 定义一个路由：当浏览器访问根路径 "/" 时，执行以下操作
     svr.Get("/", [](const httplib::Request& req, httplib::Response& res) {
-        res.set_content("<h1>Hello from C++ Web Server!</h1><p>This is your first website.</p>", "text/html");
+        res.set_content("<h1>Hello from C++ Web Server!</h1><p>This is QC first website.</p>", "text/html");
     });
 
     // 启动服务器，监听所有可用 IP 地址的 8080 端口
